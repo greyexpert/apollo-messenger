ad7182c33974dbbbf1d60f4462bc0dc089896a10
Sergey Kambalin <skambalin@klika-tech.com>
2017-09-15 12:50:08 +0300

    gifted-chat integration started

--- src/modules/messenger/components/ChannelScreen/ChannelScreen.js
+++ src/modules/messenger/components/ChannelScreen/ChannelScreen.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import { GiftedChat } from 'react-native-gifted-chat';
 import styled from 'styled-components/native';
 import {
   Container,
@@ -11,13 +13,71 @@ import {
   Icon,
   Body,
   Right,
-  Title
+  Title,
+  View
 } from 'native-base';
+
+const Wrapper = styled(View)`
+  flex: 1;
+`;
 
 export default class ChannelScreen extends Component {
   static navigationOptions = {
     header: null,
   };
+
+  state = {
+    messages: []
+  };
+
+  onSend(messages = []) {
+
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+
+    // for demo purpose
+    this.answerDemo(messages);
+  }
+
+  answerDemo(messages) {
+    if (messages.length) {
+      this.onReceive(messages[0].text);
+    }
+  }
+
+  onReceive(text) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, {
+          _id: Math.round(Math.random() * 1000000),
+          text: text,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Roma',
+            // avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        }),
+      };
+    });
+  }
+
+  renderChat() {
+
+    const { messages } = this.state;
+
+    return (
+      <GiftedChat messages={messages}
+        onSend={(messages) => this.onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+    )
+  }
 
   render() {
     const {
@@ -47,13 +107,13 @@ export default class ChannelScreen extends Component {
 
           <Right/>
         </Header>
-        <Content>
+        <Wrapper>
           {
-            loading && (
+            loading ? (
               <Spinner color='blue' />
-            )
+            ) : this.renderChat()
           }
-        </Content>
+        </Wrapper>
       </Container>
     );
   }
